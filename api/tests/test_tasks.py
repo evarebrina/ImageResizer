19d99c403d929b80aac961d18975aa07b757df4e
vadimrebrin <vadimrebrin@outlook.com>
2019-07-14 21:10:31 +0300

    Add ClearDatabase test class

--- api/tests/test_tasks.py
+++ api/tests/test_tasks.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 import uuid
 import os
 from PIL import Image
-from api.tasks import handle_image
+import datetime
+from api.tasks import handle_image, clear_database
+from api import models
 import logging
 logger = logging.getLogger('api.tests.test_tasks')
 
@@ -39,3 +41,37 @@ class HandleImage(TestCase):
         invalid_image.close()
         with self.assertRaisesMessage(Exception, 'File is not a valid image'):
             handle_image(self.resize_id, self.test_original_image_path, self.extension, 100, 100)
+
+
+class ClearDatabase(TestCase):
+
+    def setUp(self):
+        self.resize_id = str(uuid.uuid4())
+        self.extension = '.jpg'
+        image_name = self.resize_id + self.extension
+        self.original_path = 'static/original_images/'
+        self.sized_path = 'static/sized_images/'
+        self.original_image_path = self.original_path + 'original_' + image_name
+        self.sized_image_path = self.sized_path + 'sized_' + image_name
+        f = open(self.original_image_path, 'w+')
+        f.close()
+        f = open(self.sized_image_path, 'w+')
+        f.close()
+        date = datetime.datetime.now() - datetime.timedelta(minutes=15)
+        model = models.Task(uuid=self.resize_id, extension=self.extension)
+        model.save()
+        model.date = date
+        model.save()
+
+    def tearDown(self):
+        if os.path.exists(self.original_image_path):
+            os.remove(self.original_image_path)
+        if os.path.exists(self.sized_image_path):
+            os.remove(self.sized_image_path)
+
+    def test_good(self):
+        clear_database()
+
+        self.assertFalse(models.Task.objects.filter(uuid=self.resize_id).exists())
+        self.assertFalse(os.path.exists(self.original_image_path))
+        self.assertFalse(os.path.exists(self.sized_image_path))
